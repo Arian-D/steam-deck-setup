7604f7ff728d96409d1fc37fb24cea0bcb3ed46d
Arian-D <ArianXDehghani@gmail.com>
2022-09-17 09:56:04 -0700

    Add EmuDeck installer

--- run.sh
+++ run.sh
@@ -3,14 +3,11 @@
 # Variables
 deck_ip=192.168.0.9
 
-
-
 # Ping
 if ! ping -q -c 1 -W 1 $deck_ip > /dev/null; then
     echo "Deck is not reachable at $deck_ip"
     exit 1
 fi
-
 
 # Create and enter virtual environment
 if ! [ -d .venv ]; then
@@ -20,7 +17,6 @@ source .venv/bin/activate
 
 # Install ansible if it isn't installed
 pip3 install ansible
-
 
 # Run playbooks
 .venv/bin/ansible-playbook ./playbooks/emulation.yaml
